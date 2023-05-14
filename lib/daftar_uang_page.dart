108432f1cd140c8b7b859037d1019a7232001ca0
yuulye <yuulye@gmail.com>
2023-05-14 16:35:08 +0700

    starting uang code

---
+++ lib/daftar_uang_page.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class DaftarUangPage extends StatefulWidget {
+  @override
+  State<DaftarUangPage> createState() => _DaftarUangPageState();
+}
+
+class _DaftarUangPageState extends State<DaftarUangPage> {
+  List _dataUang = [];
+
+  Future<void> loadData() async {
+    final String response =
+        await rootBundle.loadString('assets/data/uang.json');
+    final data = await json.decode(response);
+    setState(() {
+      _dataUang = data;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daftar Uang'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            _dataUang.isNotEmpty
+            ? Expanded(
+              child: ListView.builder(
+                itemCount: _dataUang.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      leading: ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minWidth: 128,
+                          maxWidth: 512,
+                          maxHeight: 256,
+                        ),
+                        child: Image(
+                          image: AssetImage(
+                            'assets/images/uang/uang-$index.jpg'
+                          )
+                        ),
+                      ),
+                      title: Text(_dataUang[index]['judul']),
+                      subtitle: _dataUang[index]['tipe'] == 'Khusus'
+                      ? Text('Khusus')
+                      : Container(),
+                    ),
+                  );
+                }
+              )
+            )
+            : Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'daftar_uang_page.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
@@ -9,39 +11,101 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  var selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    var colorScheme = Theme.of(context).colorScheme;
+
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = Placeholder();
+        break;
+      case 1:
+        page = DaftarUangPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    var mainArea = ColoredBox(
+      color: colorScheme.surfaceVariant,
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 200),
+        child: page,
+      ),
+    );
+
+    List menu = [
+      Menu('Beranda'),
+      Menu('Daftar Uang'),
+    ];
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 450) {
+            return Column(
+              children: [
+                Expanded(child: mainArea),
+                SafeArea(
+                  child: BottomNavigationBar(
+                    items: [
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.home),
+                        label: menu[0].title,
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.list),
+                        label: menu[1].title,
+                      ),
+                    ],
+                    currentIndex: selectedIndex,
+                    onTap: (value) {
+                      setState(() {
+                        selectedIndex = value;
+                      });
+                    },
+                  ),
+                )
+              ],
+            );
+          } else {
+            return Row(
+              children: [
+                SafeArea(
+                  child: NavigationRail(
+                    extended: constraints.maxWidth >= 600,
+                    destinations: [
+                      NavigationRailDestination(
+                        icon: Icon(Icons.home),
+                        label: menu[0].title,
+                      ),
+                      NavigationRailDestination(
+                        icon: Icon(Icons.list),
+                        label: menu[1].title,
+                      ),
+                    ],
+                    selectedIndex: selectedIndex,
+                    onDestinationSelected: (value) {
+                      setState(() {
+                        selectedIndex = value;
+                      });
+                    },
+                  ),
+                ),
+                Expanded(child: mainArea),
+              ],
+            );
+          }
+        },
       ),
     );
   }
+}
+
+class Menu {
+  String title;
+  Menu(this.title);
 }
